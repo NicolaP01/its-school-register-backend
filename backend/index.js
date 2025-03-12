68500f8f932f92753243960c135e2d7f07a01fc6
unknown <nicola.picchio@itssmartacademy.it>
2025-03-12 17:11:55 +0100

    added api requests for courses

--- backend/index.js
+++ backend/index.js
@@ -1,156 +1,257 @@
-const crypto = require('crypto');
-const express = require('express');
-var cors = require('cors');
-const jwt= require('jsonwebtoken');
-const app = express();
-var bodyParser = require('body-parser');
-const con = require('./connector.js');
-const port = 3000;
+const crypto = require('crypto')
+const express = require('express')
+var cors = require('cors')
+const app = express()
+var bodyParser = require('body-parser')
+const port = 3000
+const con = require('./connector');
+const jwt = require('jsonwebtoken');
 
-//generate secret key (only 1 time)
-/*let secreykey = require('crypto').randomBytes(64).toString('hex');
-console.log(secreykey);*/  
+//generate jwt secret key (one time - copy to env variables)
+//let secreykey = require('crypto').randomBytes(64).toString('hex');
+//console.log(secreykey);
 
 var jsonParser = bodyParser.json()
 
 var corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 }
 
 // Enable CORS for all routes
 app.use(cors(corsOptions));
 
-app.post('/login', jsonParser, async (req, res) => {
-  let requestbody = req.body;
-  try {
-    var hash = crypto.createHash('sha256').update(requestbody.password).digest('hex');
-    const data = await con.execute(`select id from users where email = ? and password = ? and active = 1`, [requestbody.email, hash]);
-    if (data[0].length < 1) {
-      res.json({ error: true, errormessage: "INVALID_USERPWD" });
-    }
-    else {
-      const token=generateAccessToken({username: requestbody.email});
-      res.json({ error: false, errormessage: "", token: token });
-    }
-  } catch (err) {
-    console.log(err);
-    res.json({ error: true, errormessage: "GENERIC_ERROR" });
-  }
-});
-
-app.post('/createuser', authenticateToken, jsonParser, async (req, res) => {
-
-  let requestbody = req.body;
-
-  // user validation
-  try {
-    const validation = await con.execute('select * from users where fiscalcode = ?', [requestbody.fiscalcode]);
-
-    if (validation[0].length < 1) {
-      var hash = crypto.createHash('sha256').update(requestbody.password).digest('hex');
-      // user creation
-      const [data] = await con.execute(`insert into users (password, lastname, firstname, phone, email, active, fiscalcode) values (?,?,?,?,?,?,?)`, [hash, requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.email, requestbody.active, requestbody.fiscalcode]);
-      res.json(data);
-    }
-    else {
-      res.json({ error: true, errormessage: "FISCALCODE_EXISTS" });
-    }
-
-
-  } catch (err) {
-    console.log(err);
-    res.json({ error: true, errormessage: "GENERIC_ERROR" });
-  }
-});
-
-app.delete('/deleteuser/:id', authenticateToken, async (req, res) => {
-  let deleteid = req.params.id;
-
-  try {
-    const data = await con.execute('delete from users where id = ?', [deleteid]);
-    if (data[0].length < 1) {
-      res.json({ error: true, errormessage: "INVALID_ID" });
-    }
-    else{
-      res.json(data);
-    }
-  } catch (err) {
-    console.error(err);
-    res.json({ error: true, errormessage: "DELETE_ERROR" });
-  }
-})
-
-
 app.get('/getallusers', authenticateToken, async (req, res) => {
-  let startvalue = (req.query.startvalue - 1) * req.query.endvalue;
-  let endvalue = (req.query.startvalue * req.query.endvalue) - 1;
+  pagenumber = (req.query.pagenumber - 1) * req.query.pagesize;
+  pagesize = (req.query.pagenumber * req.query.pagesize) - 1;
   try {
-    const [data] = await con.execute(`select * from users LIMIT ${startvalue},${endvalue}`);
-    data.forEach((row) => {
-      console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
-    });
+    const [data] = await con.execute(`select * from users LIMIT ${pagenumber},${pagesize}`);
+    // data.forEach( (row) => {
+    //   console.log(`${row.id} = ${row.lastname} ${row.firstname} - ${row.email}`);
+    // });
 
     res.json(data);
   } catch (err) {
-    console.log(err);
-    res.json({ error: true, errormessage: "retrieve users error" });
+    console.log("Getallusers Error:" + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
   }
 })
 
 app.get('/getuser', authenticateToken, async (req, res) => {
-  
   try {
-    const [data] = await con.execute(`select * from users where id = ? LIMIT 1`, [req.query.id]);
-    
-    data.forEach((row) => {
-      console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
-    });
-
+    const [data] = await con.execute(`select * from users where id = ? LIMIT 1`, [req.user.userid]);
+    console.log(req.user.userid + " = " + req.user.username)
     res.json(data);
-
   } catch (err) {
     console.log("Getuser Error:" + err);
-    res.json({ error: true, errormessage: "GENERIC_ERROR"});
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
   }
 })
 
-app.post('/Register', jsonParser, (req, res) => {
-  console.log(req.body);
-  res.statusCode = 200;
-  res.send('Register')
+app.post('/createuser', jsonParser, authenticateToken, async (req, res) => {
+  let requestbody = req.body;
+  try {
+
+    //user validation
+    const validation = await con.query(`select id from users where fiscalcode = ?`, [requestbody.fiscalcode]);
+    if (validation[0].length < 1) {
+      var hash = crypto.createHash('sha256').update(requestbody.password).digest('hex');
+      //user creation
+      const [data] = await con.execute(`insert into users (password,lastname,firstname,phone,email,active,fiscalcode) values (?,?,?,?,?,?,?)`, [hash, requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.email, requestbody.active, requestbody.fiscalcode]);
+      res.json(data);
+    } else {
+      res.json({ error: true, errormessage: "FISCALCODE_EXISTS" });
+    }
+
+  } catch (err) {
+    console.log("Createuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
 })
 
+app.post('/login', jsonParser, async (req, res) => {
+  let requestbody = req.body;
+  try {
+    var hash = crypto.createHash('sha256').update(requestbody.pwd).digest('hex');
+    const [data] = await con.query(`select u.id, urc.id_role, urc.id_course from users u
+            inner join users_roles_courses urc on urc.id_user = u.id
+            where u.email = ? and u.password = ? and u.active = 1 `, [requestbody.email, hash]);
+    if (data[0].length < 1) {
+      res.json({ error: true, errormessage: "INVALID_USERPWD" });
+    } else {
+      const payload = { username: requestbody.email, userid: data[0]["id"], roles: data };
+      const token = generateAccessToken(payload);
+      res.json({ error: false, errormessage: "", token: token });
+    }
+  } catch (err) {
+    console.log("Login Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+})
+
+app.patch('/updateuser/:id', jsonParser, authenticateToken, async (req, res) => {
+  let patchid = req.params.id;
+  let requestbody = req.body;
+  try {
+
+   //data validation
+   const validation = await con.query(`select id from users where id = ?`, [patchid]);
+   if (validation[0].length < 1) {
+     res.json({ error: true, errormessage: "INVALID_USER_ID" });
+     return;
+   }
+
+   //update user
+   const data = await con.execute(`update users set lastname = ?, firstname = ?, phone = ?, email = ?, active = ?, fiscalcode = ? where id = ?`, [requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.email, requestbody.active, requestbody.fiscalcode, patchid]);
+   res.json(data);
+
+  } catch (err) {
+    console.log("Updateuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
+})
+
+app.post('/updatepwd', jsonParser, authenticateToken, async (req, res) => {
+  let requestbody = req.body;
+  try {
+
+   //data validation
+   const validation = await con.query(`select id from users where email = ?`, [requestbody.email]);
+   if (validation[0].length < 1) {
+     res.json({ error: true, errormessage: "INVALID_USER" });
+     return;
+   }
+
+   //update user password
+   var hash = crypto.createHash('sha256').update(requestbody.password).digest('hex');
+   const data = await con.execute(`update users set password = ? where id = ?`, [hash, validation[0][0].id]);
+   res.json(data);
+
+  } catch (err) {
+    console.log("Updatepwd Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
+})
+
+app.delete('/deleteuser/:id', authenticateToken, async (req, res) => {
+  let deleteid = req.params.id;
+  try {
+
+    //data validation
+    const validation = await con.query(`select id from users where id = ?`, [deleteid]);
+    if (validation[0].length < 1) {
+      res.json({ error: true, errormessage: "INVALID_USER_ID" });
+      return;
+    }
+
+    //delete user
+    const data = await con.execute(`delete from users where id = ?`, [deleteid]);
+    res.json(data);
+  } catch (err) {
+    console.log("Deleteuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
+})
+
+app.post('/createcourse', jsonParser, authenticateToken, async (req, res) => {
+  let requestbody = req.body;
+  try {
+
+    //user validation
+    const validation = await con.query(`select name, year from courses where name = ? and year = ?`, [requestbody.name, requestbody.year]);
+    if (validation[0].length < 1) {
+      //user creation
+      const [data] = await con.execute(`insert into courses (name,year,period) values (?,?,?)`, [requestbody.name, requestbody.year, requestbody.period]);
+      res.json(data);
+    } else {
+      res.json({ error: true, errormessage: "COURSE_EXISTS" });
+    }
+
+  } catch (err) {
+    console.log("Createuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
+})
+
+app.delete('/deletecourse/:id', authenticateToken, async (req, res) => {
+  let deleteid = req.params.id;
+  try {
+
+    //data validation
+    const validation = await con.query(`select id from courses where id = ?`, [deleteid]);
+    if (validation[0].length < 1) {
+      res.json({ error: true, errormessage: "INVALID_COURSE_ID" });
+      return;
+    }
+
+    //delete user
+    const data = await con.execute(`delete from courses where id = ?`, [deleteid]);
+    res.json(data);
+  } catch (err) {
+    console.log("Deleteuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+
+})
 
 app.listen(port, () => {
-  console.log(`Its register app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
 
-// inizializzazione connessione
-/*const connection = mysql.createConnection({
-    host     : '127.0.0.1', // macchina locale, localhost == 127.0.0.1
-    port     : 3306,
-    user     : 'its_user',
-    password : 'its@123456789',
-    database : 'registro'
-});
+app.patch('/updatecourse/:id', jsonParser, authenticateToken, async (req, res) => {
+  let patchid = req.params.id;
+  let requestbody = req.body;
+  try {
 
-connection.connect(function (err) {
-  // Check if there is a connection error
-  if (err) {
-      console.log("connection error", err.stack);
-      return;
+   //data validation
+   const validation = await con.query(`select id from courses where id = ?`, [patchid]);
+   if (validation[0].length < 1) {
+     res.json({ error: true, errormessage: "INVALID_USER_ID" });
+     return;
+   }
+
+   //update user
+   const data = await con.execute(`update courses set name = ?, year = ?, period = ? where id = ?`, [requestbody.name, requestbody.year, requestbody.period, patchid]);
+   res.json(data);
+
+  } catch (err) {
+    console.log("Updateuser Error: " + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
   }
 
-  console.log(`connected to database`);
-});
+})
 
-connection.end((err) => {
-  // The connection is terminated gracefully
-  // Ensures all remaining queries are executed
-  // Then sends a quit packet to the MySQL server.
-});
-*/
+app.get('/getallcourses', authenticateToken, async (req, res) => {
+  startvalue = (req.query.startvalue - 1) * req.query.endvalue;
+  endvalue = (req.query.startvalue * req.query.endvalue) - 1;
+  try {
+    const [data] = await con.execute(`select * from courses LIMIT ${startvalue},${endvalue}`);
+    // data.forEach( (row) => {
+    //   console.log(`${row.id} = ${row.lastname} ${row.firstname} - ${row.email}`);
+    // });
+
+    res.json(data);
+  } catch (err) {
+    console.log("Getallusers Error:" + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+})
+
+app.get('/getusercourse', authenticateToken, async (req, res) => {
+  try {
+    const [data] = await con.execute(`select distinct c.* from users_roles_courses urc inner join courses c on urc.id_course=c.id where urc.id_user = ? LIMIT 1`, [req.user.userid]);
+    res.json(data);
+  } catch (err) {
+    console.log("Getuser Error:" + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+})
+
 
 function generateAccessToken(username) {
   return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
